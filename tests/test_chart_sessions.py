1ed06e9d2e15ef7d553d39cb24bbe499b21f4d0c
Mohamed Amr <mohamedamr2748@gmail.com>
2026-09-14 18:48:29 +0800

    Tighten chart session tests

--- tests/test_chart_sessions.py
+++ tests/test_chart_sessions.py
@@ -3,24 +3,6 @@ import pandas as pd
 from app import charts
 
 
-def _frame():
-    idx = pd.date_range("2026-09-11 08:00", periods=5, freq="15min", tz="America/New_York").tz_convert("UTC")
-    return pd.DataFrame({
-        "Open": [100, 101, 102, 103, 104],
-        "High": [101, 102, 103, 104, 105],
-        "Low": [99, 100, 101, 102, 103],
-        "Close": [100, 101, 102, 103, 104],
-        "Volume": [1, 1, 1, 1, 1],
-    }, index=idx)
-
-
-def test_display_index_converts_aware_stock_timestamps_to_new_york():
-    frame = _frame()
-    displayed = charts._display_index(frame.index, "AAPL")
-    assert displayed[0].hour == 8
-    assert displayed[0].tz is None
-
-
 def test_regular_session_mask_uses_new_york_time():
     idx = pd.to_datetime([
         "2026-09-11 08:00-04:00",
